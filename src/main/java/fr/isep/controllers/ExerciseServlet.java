8466011179080b48417eda6aa967771ed9565465
Patrick Wang <patrick.wang@isep.fr>
2018-11-26 18:48:13 +0100

    Update for exercise logs

--- src/main/java/fr/isep/controllers/ExerciseServlet.java
+++ src/main/java/fr/isep/controllers/ExerciseServlet.java
@@ -1,4 +1,54 @@
 package fr.isep.controllers;
 
-public class ExerciseServlet {
+import com.google.gson.Gson;
+import fr.isep.helpers.URIHelper;
+import fr.isep.models.ExerciseLog;
+import fr.isep.models.dao.LogDataSource;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.stream.Collectors;
+
+@WebServlet(name = "ExerciseServlet", urlPatterns = {URIHelper.CHANGE_EXERCISE, URIHelper.RUN_EXERCISE})
+public class ExerciseServlet extends HttpServlet {
+    private LogDataSource dataSource = new LogDataSource();
+
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        switch (req.getRequestURI()) {
+            case URIHelper.BASE_URI + URIHelper.CHANGE_EXERCISE:
+                changeExercise(req, resp);
+                break;
+            case URIHelper.BASE_URI + URIHelper.RUN_EXERCISE:
+                break;
+            default:
+                throw new ServletException("An error occurred while processing your request.");
+        }
+    }
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        RequestDispatcher dispatcher = req.getRequestDispatcher("404.html");
+        dispatcher.forward(req, resp);
+    }
+
+    private void changeExercise(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        Gson gson = new Gson();
+        ExerciseLog exerciseLog = gson.fromJson(request.getReader().lines().collect(Collectors.joining(System.lineSeparator())), ExerciseLog.class);
+        try {
+            String workspaceState = dataSource.changeExercise(exerciseLog);
+            response.getWriter().write(workspaceState);
+        } catch (SQLException e) {
+            System.out.println("An error occurred while trying to change exercise");
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private void runExercise(HttpServletRequest request, HttpServletResponse response) {
+        // TODO: To implement
+    }
 }
--- src/main/java/fr/isep/models/ExerciseLog.java
+++ src/main/java/fr/isep/models/ExerciseLog.java
@@ -1,4 +1,60 @@
 package fr.isep.models;
 
 public class ExerciseLog {
+    public static final String WORKSPACE_XML = "xml";
+
+    private String userID;
+    private int currentExerciseID;
+    private String newExerciseID;
+    private String action;
+    private String workspacexml;
+    private String time;
+
+    public String getNewExerciseID() {
+        return newExerciseID;
+    }
+
+    public void setNewExerciseID(String newExerciseID) {
+        this.newExerciseID = newExerciseID;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public int getCurrentExerciseID() {
+        return currentExerciseID;
+    }
+
+    public void setCurrentExerciseID(int currentExerciseID) {
+        this.currentExerciseID = currentExerciseID;
+    }
+
+    public String getAction() {
+        return action;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
+    }
+
+    public String getWorkspacexml() {
+        return workspacexml;
+    }
+
+    public void setWorkspacexml(String workspacexml) {
+        this.workspacexml = workspacexml;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
 }
